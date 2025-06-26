e91fcd1d806b042638d1dbcf8f6cc0d5334a04d0
dyhia-ouldtaleb <dyhia.ouldtaleb@ecoles-epsi.net>
2025-06-26 19:48:09 +0200

    Ajout du header minimaliste et configuration des fichiers statiques

--- meteo_site/settings.py
+++ meteo_site/settings.py
@@ -117,8 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'meteo_app/static']  # ← Pour le développement (ton CSS, etc.)
+STATIC_ROOT = BASE_DIR / 'staticfiles'              # ← Pour la commande `collectstatic` en production
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
